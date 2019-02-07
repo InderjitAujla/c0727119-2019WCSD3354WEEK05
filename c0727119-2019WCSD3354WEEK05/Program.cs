91c57e3e548afc1fbbb14a4a8559c8bdff00b207
visualstudio <visualstudio@230-PC39>
2019-02-07 15:16:38 -0500

    3:16 commit code

---
+++ c0727119-2019WCSD3354WEEK05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c0727119_2019WCSD3354WEEK05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
